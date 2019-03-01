19c7bf7d498d8bd8359028bfe57912d340fe800f
paulpan <pandingyuan@beyondsoft.com>
2019-03-01 17:01:55 +0800

    u

---
+++ src/com/mcnz/restful/java/exampleTest/AppTest.java
@@ -0,0 +1,43 @@
+package com.mcnz.restful.java.exampleTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AppTest {
+
+    private WebDriver driver;
+
+    @BeforeTest
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        capabilities.setPlatform(Platform.LINUX);
+        driver = new RemoteWebDriver(new URL("https://selenium.devops.sparkflow.top/wd/hub"), capabilities);
+    }
+
+    @AfterTest
+    public void afterTest() {
+        driver.quit();
+    }
+
+    @Test
+    public void simpleTest() {
+        driver.navigate().to("http://www.google.com");
+        Assert.assertEquals(driver.getTitle(), "Google");
+    }
+    @Test
+    public void simpleTest1() {
+        driver.navigate().to("http://goodv1.us-west-2.elasticbeanstalk.com/Roo/");
+        Assert.assertEquals(driver.findElement(By.xpath("/html/body/a")).getText(), "RunRestApp");
+		Assert.assertEquals(driver.findElement(By.xpath("/html/body/h1")).getText(), "RestServiceDeploy");
+    }
+}
